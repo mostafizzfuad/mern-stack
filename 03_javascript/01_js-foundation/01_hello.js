1af2eefab1f77709c6ccd5e811881fa303360657
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-05-27 11:47:34 +0600

    update code

---
+++ 03_javascript/01_js-foundation/01_hello.js
@@ -0,0 +1,17 @@
+// JS : JS is a high level , multi paradine, interprated, object oriented, functional programming language.
+
+console.log("Hello JS")
+console.log('Hello JS')
+console.log(`Hello JS`)
+
+// variables : let varName = value;  [don't use var keyword for declaration a variable & use camelCase for variable names]
+let myName = 'Mostafizur Rahman'
+console.log('Hello ' + myName) // string concatenation
+console.log(`Hello ${myName}`) // template literal : backtick
+
+
+// data types : (value has type, not variable)
+
+// typeof varName : (variable er datatype janar jonno use kora hoi)
+
+// kono variable er value null , but typeof diye sei variable check korle dataType dekhabe object. this is a bug.
